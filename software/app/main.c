704db84f48625c140b699e660b5629167f16ae32
Ahmet KÜRKLÜ <ahmetkurklu2000@gmail.com>
2023-01-13 17:25:42 +0100

    Fin de séance affichage de Z0 et Z1

--- software/app/main.c
+++ software/app/main.c
@@ -15,18 +15,29 @@
 
 
 //Fonction pour lire avec l'I2C
-void lecture_i2c(int base,int addr){
+int lecture_i2c(int base,int addr){
 	
+	int data = 0;
 	I2C_start(base, ALT_ADDR, 0);
 	I2C_write(base,addr,0);
-	I2C_start(base,ALT_ADDR,1);
 	
-	alt_printf("%x\n",I2C_read(base,0));
+	I2C_start(base,ALT_ADDR,1);
+	data = I2C_read(base,1);
+	
+	return data;
 }
+
+void ecriture_i2c(int base,int addr,int value){
+	I2C_start(base, ALT_ADDR, 0);
+	I2C_write(base,addr,0);
+	I2C_write(base,value,1);
+}
+
 
 
 int main(){
 	alt_printf("\n\n\n\nDans le main\n\n");
+	int data =0;
 	
 	//Initialisation de l'i2c
 	alt_printf("Initialisation I2C\n");
@@ -36,15 +47,37 @@ int main(){
 		alt_printf("Start ok\n");
 	}
 	
-	
 	//Lecture ACT_INACT_CTL
 	alt_printf("Lecture ACT_INACT_CTL\n");
-	lecture_i2c(OPENCORES_I2C_0_BASE,ACT_INACT_CTL);
+	data = lecture_i2c(OPENCORES_I2C_0_BASE,ACT_INACT_CTL);
+	alt_printf("ACT_INACT_CTL = %x\n",data);
 	
 	//Lecture POWER_CTL
 	alt_printf("Lecture POWER_CTL\n");
-	lecture_i2c(OPENCORES_I2C_0_BASE,POWER_CTL);
+	data = lecture_i2c(OPENCORES_I2C_0_BASE,POWER_CTL);
+	alt_printf("POWER_CTL = %x\n",data);
+		
+	//Ecriture POWER_CTL
+	alt_printf("Ecriture POWER_CTL\n");
+	ecriture_i2c(OPENCORES_I2C_0_BASE,POWER_CTL,0x8);
+	usleep(100000);
+	
+	//Lecture POWER_CTL
+	alt_printf("Lecture POWER_CTL\n");
+	data = lecture_i2c(OPENCORES_I2C_0_BASE,POWER_CTL);
+	alt_printf("POWER_CTL = %x\n",data);
+	
+	
+	//Lecture Z0
+	alt_printf("Lecture Z0\n");
+	data = lecture_i2c(OPENCORES_I2C_0_BASE,0x36);
+	alt_printf("Z0 = %x\n",data);
+	//Lecture Z1
+	alt_printf("Lecture Z1\n");
+	data = lecture_i2c(OPENCORES_I2C_0_BASE,0x37);
+	alt_printf("Z1 = %x\n",data);
 	
 	while(1){
+		
 	}
 }
