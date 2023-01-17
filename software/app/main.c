4825336d0e88525739e28b0b73108429d97daf42
Ahmet KÜRKLÜ <ahmetkurklu2000@gmail.com>
2023-01-17 14:52:11 +0100

    Affichage par polling de Z validé

--- software/app/main.c
+++ software/app/main.c
@@ -38,7 +38,6 @@
 
 
 
-
 //Fonction pour lire avec l'I2C
 int lecture_i2c(int base,int addr){
 	
@@ -52,34 +51,45 @@ int lecture_i2c(int base,int addr){
 	return data;
 }
 
+//Fonction pour ecrire avec l'I2C
 void ecriture_i2c(int base,int addr,int value){
 	I2C_start(base, ALT_ADDR, 0);
 	I2C_write(base,addr,0);
 	I2C_write(base,value,1);
 }
 
-
+//Fonction pour la calibration 
 void calibration(){
 	//Ecriture OFSZ
-	ecriture_i2c(OPENCORES_I2C_0_BASE,OFSZ,CALIBZ);
+	ecriture_i2c(OPENCORES_I2C_0_BASE,OFSZ,0);
 	usleep(100000);
 	
 	//Ecriture OFSX
-	ecriture_i2c(OPENCORES_I2C_0_BASE,OFSX,CALIBX);
+	ecriture_i2c(OPENCORES_I2C_0_BASE,OFSX,0);
 	usleep(100000);
 	
 	//Ecriture OFSY
-	ecriture_i2c(OPENCORES_I2C_0_BASE,OFSY,CALIBY);
+	ecriture_i2c(OPENCORES_I2C_0_BASE,OFSY,0);
 	usleep(100000);
 	
 }
-
 
 //Fonction qui prend un entier et le decoupe et ecrit sur les registres des 7 segments
 void int_to_seg(int nbr){
 	int i =0;
 	int tab[5] = {0,0,0,0,0};
-
+	//nbr = (short)nbr;
+	
+	if(nbr < 0){
+		IOWR_ALTERA_AVALON_PIO_DATA(SEG5_BASE,0b0111111);
+		nbr = nbr * -1;
+		
+	}
+	else{
+		IOWR_ALTERA_AVALON_PIO_DATA(SEG5_BASE,0b1000000);
+		
+	}
+	
 	while (nbr >= 10) {
 		tab[i] = nbr % 10;
 		nbr = nbr / 10;
@@ -95,96 +105,79 @@ void int_to_seg(int nbr){
 
 }
 
-void affichage_Z(){
-	int z0,z1,z;
+// Prend LSB MSB et donne le data final pour l'affichache sur les 7seg
+void affichage_XYZ(int A0,int A1){
+	int a0,a1,a;
+	a0 = lecture_i2c(OPENCORES_I2C_0_BASE,A0);
+	a1 = lecture_i2c(OPENCORES_I2C_0_BASE,A1);
+	a = (a1<<8)|a0;
+	
+	a=	(short) a;
+	a = a*3.9;
+	
+	int_to_seg(a);
+}
+
+//Fonction pour l'affichache par UART
+void affichache_UART(){
+	int x1,x0,y1,y0,z1,z0;
+	//Lecture X0 Lecture X1
+	x0 = lecture_i2c(OPENCORES_I2C_0_BASE,X0);
+	x1 = lecture_i2c(OPENCORES_I2C_0_BASE,X1);
+	//Lecture Y0 Lecture Y1
+	y0 = lecture_i2c(OPENCORES_I2C_0_BASE,Y0);
+	y1 = lecture_i2c(OPENCORES_I2C_0_BASE,Y1);
 	//Lecture Z0 Lecture Z1
 	z0 = lecture_i2c(OPENCORES_I2C_0_BASE,Z0);
 	z1 = lecture_i2c(OPENCORES_I2C_0_BASE,Z1);
-	z = (z1<<8)|z0;
 	
-	//to signed
-	signed int zz = z;
+	alt_printf("X= %x, Y= %x, Z= %x\n",(x1<<8)|x0,(y1<<8)|y0,(z1<<8)|z0);
+	alt_printf("--------------------\n");
+}
+
+//Fonction pour l'initialisation du ADXL345
+void init_ADXL345(){
 	
-	z = z*3.9;
-	z = (int)z;
+	alt_printf("Initialisation I2C\n");
+	I2C_init(OPENCORES_I2C_0_BASE, ALT_CPU_FREQ, SPEED);
+	if(I2C_start(OPENCORES_I2C_0_BASE, ALT_ADDR, 0)== 0){
+		
+		alt_printf("Init ok\n");
+	}
+	//Ecriture POWER_CTL
+	alt_printf("Ecriture 0X08 -> POWER_CTL\n");
+	ecriture_i2c(OPENCORES_I2C_0_BASE,POWER_CTL,0x8);
+	usleep(100000);
 	
-	int_to_seg(z);
+	//Lecture POWER_CTL
+	alt_printf("Lecture POWER_CTL\n");
+	alt_printf("POWER_CTL = %x\n",lecture_i2c(OPENCORES_I2C_0_BASE,POWER_CTL));
+	
+	//D1=1 et D0=1 -> +-16g 
+	//Ecriture DATA_FORMAT
+	alt_printf("Ecriture 0x07 -> DATA_FORMAT\n");
+	ecriture_i2c(OPENCORES_I2C_0_BASE,DATA_FORMAT,0x7);
+	usleep(100000);
+	
+	//Lecture DATA_FORMAT
+	alt_printf("Lecture DATA_FORMAT\n");
+	alt_printf("DATA_FORMAT = %x\n\n",lecture_i2c(OPENCORES_I2C_0_BASE,DATA_FORMAT));
 	
 }
 
 
 int main(){
 	alt_printf("\n\n\n\nDans le main\n\n");
-	int data =0;
 	
 	//Initialisation de l'i2c
-	alt_printf("Initialisation I2C\n");
-	I2C_init(OPENCORES_I2C_0_BASE, ALT_CPU_FREQ, SPEED);
-	if(I2C_start(OPENCORES_I2C_0_BASE, ALT_ADDR, 0)== 0){
-		
-		alt_printf("Start ok\n");
-	}
+	init_ADXL345();
 	
-	//Lecture ACT_INACT_CTL
-	alt_printf("Lecture ACT_INACT_CTL\n");
-	data = lecture_i2c(OPENCORES_I2C_0_BASE,ACT_INACT_CTL);
-	alt_printf("ACT_INACT_CTL = %x\n",data);
-	
-	//Lecture POWER_CTL
-	alt_printf("Lecture POWER_CTL\n");
-	data = lecture_i2c(OPENCORES_I2C_0_BASE,POWER_CTL);
-	alt_printf("POWER_CTL = %x\n",data);
-		
-	//Ecriture POWER_CTL
-	alt_printf("Ecriture POWER_CTL\n\n");
-	ecriture_i2c(OPENCORES_I2C_0_BASE,POWER_CTL,0x8);
-	usleep(100000);
-	
-	//Lecture POWER_CTL
-	alt_printf("Lecture POWER_CTL\n");
-	data = lecture_i2c(OPENCORES_I2C_0_BASE,POWER_CTL);
-	alt_printf("POWER_CTL = %x\n",data);
-	
-	
-	//Lecture DATA_FORMAT
-	alt_printf("Lecture DATA_FORMAT\n");
-	data = lecture_i2c(OPENCORES_I2C_0_BASE,DATA_FORMAT);
-	alt_printf("DATA_FORMAT = %x\n",data);
-	
-	//D1=1 et D0=1 -> +-16g 
-	//Ecriture DATA_FORMAT
-	alt_printf("Ecriture DATA_FORMAT\n\n");
-	ecriture_i2c(OPENCORES_I2C_0_BASE,DATA_FORMAT,0x7);
-	usleep(100000);
-	
-	//Lecture DATA_FORMAT
-	alt_printf("Lecture DATA_FORMAT\n");
-	data = lecture_i2c(OPENCORES_I2C_0_BASE,DATA_FORMAT);
-	alt_printf("DATA_FORMAT = %x\n",data);
-	
+	//Calibration
 	calibration();
-
-	
-	int x1,x0,y1,y0,z1,z0;
 	while(1){
-		/*
-		//Lecture X0 Lecture X1
-		x0 = lecture_i2c(OPENCORES_I2C_0_BASE,X0);
-		x1 = lecture_i2c(OPENCORES_I2C_0_BASE,X1);
-		//Lecture Y0 Lecture Y1
-		y0 = lecture_i2c(OPENCORES_I2C_0_BASE,Y0);
-		y1 = lecture_i2c(OPENCORES_I2C_0_BASE,Y1);
-		//Lecture Z0 Lecture Z1
-		z0 = lecture_i2c(OPENCORES_I2C_0_BASE,Z0);
-		z1 = lecture_i2c(OPENCORES_I2C_0_BASE,Z1);
 		
-		alt_printf("X= %x, Y= %x, Z= %x\n",(x1<<8)|x0,(y1<<8)|y0,(z1<<8)|z0);
-		alt_printf("--------------------\n");
-		
-		*/
-		
-		affichage_Z();
-		
+		affichache_UART();
+		affichage_XYZ(Z0,Z1);
 		usleep(1000000);
 	}
 }
